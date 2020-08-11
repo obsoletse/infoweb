55a86917672a956dfd591e6253147127ef3c3891
obsoletse <1191430716@qq.com>
2020-08-12 00:58:51 +0800

    布局

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,51 +1,40 @@
 <template>
-  <el-container>
-    <el-header>Header</el-header>
-    <el-container>
-      <el-aside width="300px">Aside</el-aside>
-      <el-main>
-        <el-row :gutter="20">
-          <el-col :span="7">
-            <div class="myFormHeader">
-              <el-card class="box-card">
-                  <!--登录表单文字-->
-                  <div slot="header" class="clearfix">
-                    <span>用户登录</span>
-                  </div>
-                  <el-form ref="form" :model="form" label-width="80px">
-                  <el-form-item label="用户名">
-                    <el-input v-model="form.username"></el-input>
-                  </el-form-item>
-                  <el-form-item label="密码">
-                    <el-input v-model="form.password"></el-input>
-                  </el-form-item>
-                  <el-form-item style="margin-top: -10px">
-                   <!-- <el-row justify="center" type="flex">-->
-                      <el-col :span="8">
-                        <el-checkbox v-model="checked">记住密码</el-checkbox>
-                      </el-col>
-                      <el-col :span="16">
-                        <el-button type="text">忘记密码?</el-button>
-                      </el-col>
-                    <!--</el-row>-->
-                  </el-form-item>
-                  <el-form-item style="margin-top: -15px">
-                    <el-col :span="8">
-                      <el-button type="primary" @click="onSubmit" size="small" round plain>登录</el-button>
-                    </el-col>
-                    <el-col :span="6">
-                      <el-button size="small" round>取消</el-button>
-                    </el-col>
-                  </el-form-item>
-                </el-form>
-            </el-card>
-            </div>
-
-          </el-col>
-        </el-row>
-      </el-main>
-    </el-container>
-  </el-container>
+  <el-row :gutter="20">
+    <el-col :span="7">
+      <div class="myFormHeader">
+        <el-card class="box-card">
+          <!--登录表单文字-->
+          <div slot="header" class="clearfix">
+            <span>用户登录</span>
+          </div>
+          <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="用户名">
+            <el-input v-model="form.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input v-model="form.password"></el-input>
+          </el-form-item>
+          <el-form-item style="margin-top: -10px">
+            <el-col :span="8">
+              <el-checkbox v-model="checked">记住密码</el-checkbox>
+            </el-col>
+            <el-col :span="16">
+              <el-button type="text">忘记密码?</el-button>
+            </el-col>
+          </el-form-item>
+          <el-form-item style="margin-top: -15px">
+            <el-col :span="8">
+              <el-button type="primary" @click="onSubmit" size="small" round plain>登录</el-button>
+            </el-col>
+            <el-col :span="6">
+              <el-button size="small" round>取消</el-button>
+            </el-col>
+          </el-form-item>
+        </el-form>
+      </el-card>
+      </div>
+    </el-col>
+  </el-row>
 
 </template>
 
