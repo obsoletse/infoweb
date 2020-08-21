02816b573538241363b072693846c2dfe23d7335
obsoletse <1191430716@qq.com>
2020-08-21 09:53:23 +0800

    home页面修改

--- src/router/index.js
+++ src/router/index.js
@@ -11,24 +11,15 @@ const routes = [
   //主页面模块
   { path:'/home',component:() => import('@views/home/home'),
     children:[
-      {path: '/info', component: () => import('@views/menu/info/infoIndex')
-      },
-      {path: '/workSpace', component: () => import('@views/menu/workSpace/workSpaceIndex')
-      },
-      {path: '/question', component: () => import('@views/menu/question/questionIndex')
-      },
-      {path: '/list', component: () => import('@views/menu/list/listIndex')
-      },
-      {path: '/userInfo', component: () => import('@views/user/userInfo')
-      },
-      {path: '/help', component: () => import('@views/user/help')
-      },
-      {path: '/userMessage', component: () => import('@views/user/userMessage/userMessageIndex')
-      },
-      {path: '/userOrder', component: () => import('@views/user/userOrder/userOrderIndex')
-      },
-      {path: '/userWallet', component: () => import('@views/user/userWallet')
-      }
+      {path: '/info', component: () => import('@views/menu/info/infoIndex')},
+      {path: '/workSpace', component: () => import('@views/menu/workSpace/workSpaceIndex')},
+      {path: '/question', component: () => import('@views/menu/question/questionIndex')},
+      {path: '/list', component: () => import('@views/menu/list/listIndex')},
+      {path: '/userInfo', component: () => import('@views/user/userInfo')},
+      {path: '/help', component: () => import('@views/user/help')},
+      {path: '/userMessage', component: () => import('@views/user/userMessage/userMessageIndex')},
+      {path: '/userOrder', component: () => import('@views/user/userOrder/userOrderIndex')},
+      {path: '/userWallet', component: () => import('@views/user/userWallet')}
     ]
   }
 ]
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,25 +1,29 @@
 <template>
     <div>
         <el-menu
-                router
-                mode="horizontal"
-                background-color="white"
-                text-color="#222"
-                active-text-color="red"
-                style="min-width: 1300px">
+            router
+            mode="horizontal"
+            background-color="white"
+            text-color="#222"
+            active-text-color="red"
+            style="min-width: 1300px">
             <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name" style="left:32%">
                 {{ item.navItem }}
             </el-menu-item>
-            <div style="position: absolute;padding-top: 12px;left:10%"><el-avatar :size="38" :src="iconImag"></el-avatar></div>
-            <span style="position: absolute;padding-top: 20px;left:15%;font-size: 20px;font-weight: bold">Your Mind Palace</span>
+            <!--logo、系统名字-->
+            <div class="home_el_avatar"><el-avatar :size="38" :src="iconImag"/></div>
+            <span class="home_el_avatar_span">Your Mind Palace</span>
 
-            <div style="position: absolute;padding-top: 13px;right:15%">
-                <el-input placeholder="请输入内容" v-model="input3" class="input" style="width: 350px">
+            <!--输入框-->
+            <div class="home_header_input">
+                <el-input placeholder="请输入内容" v-model="query" class="input" style="width: 300px" size="small">
                     <el-button slot="append" icon="el-icon-search"></el-button>
                 </el-input>
             </div>
-            <div style="position: absolute;padding-top: 10px;right:5%">
-                <el-dropdown @command="handleCommand" >
+
+            <!--用户下拉菜单-->
+            <div class="home_user_el_avatar">
+                <el-dropdown @command="handleCommand">
                     <div><el-avatar :src="userImag"></el-avatar></div>
                     <el-dropdown-menu slot="dropdown">
                         <el-dropdown-item command="userInfo">我的资料</el-dropdown-item>
@@ -37,43 +41,43 @@
 
 <script>
     export default {
-            name: "home",
-            methods: {
-                handleCommand(command) {
-                    if(command=='userInfo')
-                    {
-                        this.$router.push('/userInfo')
-                    }
-                    if(command=='userWallet')
-                    {
-                        this.$router.push('/userWallet')
-                    }
-                    if(command=='userOrder')
-                    {
-                        this.$router.push('/userOrder')
-                    }
-                    if(command=='userMessage')
-                    {
-                        this.$router.push('/userMessage')
-                    }
-                    if(command=='help')
-                    {
-                        this.$router.push('/help')
-                    }
+        name: "home",
+        methods: {
+            handleCommand(command) {
+                if(command=='userInfo')
+                {
+                    this.$router.push('/userInfo')
                 }
+                if(command=='userWallet')
+                {
+                    this.$router.push('/userWallet')
+                }
+                if(command=='userOrder')
+                {
+                    this.$router.push('/userOrder')
+                }
+                if(command=='userMessage')
+                {
+                    this.$router.push('/userMessage')
+                }
+                if(command=='help')
+                {
+                    this.$router.push('/help')
+                }
+            }
 
-            },
-            data () {
-                return {
-                    navList: [
-                        {name: '/info', navItem: '消息中心'},
-                        {name: '/question', navItem: '问答'},
-                        {name: '/workSpace', navItem: '我的工作台'},
-                        {name: '/list', navItem: '排行榜'}
-                    ],
-                    iconImag: require('../../assets/logo.png'),
-                    userImag: "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",
-                    input3: ''
+        },
+        data () {
+            return {
+                navList: [
+                    {name: '/info', navItem: '消息中心'},
+                    {name: '/question', navItem: '问答'},
+                    {name: '/workSpace', navItem: '我的工作台'},
+                    {name: '/list', navItem: '排行榜'}
+                ],
+                iconImag: require('../../assets/logo.png'),
+                userImag: "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",
+                query: ''
 
                 }
             }
@@ -89,5 +93,25 @@
     span {
         pointer-events: none;
     }
+    .home_el_avatar{
+        position: absolute;
+        padding-top: 12px;
+        left:10%
+    }
+    .home_el_avatar_span{
+        position: absolute;
+        padding-top: 20px;
+        left:15%;
+        font-size: 20px;
+        font-weight: bold
+    }
+    .home_header_input{
+        position: absolute;
+        padding-top: 13px;
+        right:15%
+    }
 
+    .home_user_el_avatar{
+        position: absolute;padding-top: 10px;right:5%
+    }
 </style>
